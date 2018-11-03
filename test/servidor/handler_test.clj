3920b61079704ee72d7b9e8283506207d6f72d50
danielhvs <danielhvs70@gmail.com>
2018-11-03 01:34:38 -0200

    cadastrando backup

--- test/servidor/handler_test.clj
+++ test/servidor/handler_test.clj
@@ -1,24 +1,27 @@
 (ns servidor.handler-test
   (:require [clojure.test :refer :all]
             [ring.mock.request :as mock]
+            [clojure.edn :as edn]
             [clojure.data.json :as json]
             [servidor.handler :refer :all]))
 
 (defn tira-ids [produto]
-  (json/write-str
-   (dissoc produto :_id)))
+  (dissoc produto :_id))
+
+(def BACKUP "./backup.json")
+
+(defn chama-cadastra [produto]
+  (is (= (app (-> (mock/request :post "/cadastra")
+                  (mock/json-body produto)))
+         {:status 200})))
 
 (deftest test-app
-  (testing "main route"
-    (let [response (app (mock/request :get "/"))]
-      (is (= (:status response) 200))
-      (is (= (:body response) "Hello World"))))
   (testing "consulta tudo e guarda backup"
-    (let [response (app (mock/request :get "/consulta"))
-          resultado (json/read-str (:body response) :key-fn keyword)]
-      (is (= (:status response) 200))
-      (let [resultado (map tira-ids resultado)] 
-        (spit "./backup.json" (prn-str resultado)))))
-  (testing "not-found route"
-    (let [response (app (mock/request :get "/invalid"))]
-      (is (= (:status response) 404)))))
+      (let [response (app (mock/request :get "/consulta"))
+            resultado (json/read-str (:body response) :key-fn keyword)]
+        (is (= (:status response) 200))
+        (let [resultado (map tira-ids resultado)] 
+          (spit BACKUP (prn-str resultado)))))
+  (testing "cadastra todo backup" 
+    (let [produtos (-> BACKUP (slurp) (edn/read-string))] 
+      (doall (map chama-cadastra produtos)))))
