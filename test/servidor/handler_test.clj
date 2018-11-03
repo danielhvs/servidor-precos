80e679577d422b989b0a0b77f8047ee2835ba65e
danielhvs <danielhvs70@gmail.com>
2018-11-03 01:11:10 -0200

    guardando consulta em arquivo

--- test/servidor/handler_test.clj
+++ test/servidor/handler_test.clj
@@ -1,14 +1,24 @@
 (ns servidor.handler-test
   (:require [clojure.test :refer :all]
             [ring.mock.request :as mock]
+            [clojure.data.json :as json]
             [servidor.handler :refer :all]))
+
+(defn tira-ids [produto]
+  (json/write-str
+   (dissoc produto :_id)))
 
 (deftest test-app
   (testing "main route"
     (let [response (app (mock/request :get "/"))]
       (is (= (:status response) 200))
       (is (= (:body response) "Hello World"))))
-
+  (testing "consulta tudo e guarda backup"
+    (let [response (app (mock/request :get "/consulta"))
+          resultado (json/read-str (:body response) :key-fn keyword)]
+      (is (= (:status response) 200))
+      (let [resultado (map tira-ids resultado)] 
+        (spit "./backup.json" (prn-str resultado)))))
   (testing "not-found route"
     (let [response (app (mock/request :get "/invalid"))]
       (is (= (:status response) 404)))))
