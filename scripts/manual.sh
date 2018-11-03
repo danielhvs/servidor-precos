6d20eea5d172fa431869772a27e5a0e4eb5b543a
danielhvs <danielhvs70@gmail.com>
2018-11-03 20:21:14 -0200

    cadastrando compra com nota fiscal

---
+++ scripts/manual.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo local:
+read local
+
+while true; do
+    echo nome:
+    read nome
+    echo preco:
+    read preco
+    echo {\"nome\": \"$nome\", \"local\": \"$local\", \"preco\": \"$preco\"} >> ../resources/estoque.edn
+done
+
