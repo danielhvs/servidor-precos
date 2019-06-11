9daa649a8f9d1c939e8adcd5d41f1707133189d2
danielhvs <danielh70@gmail.com>
2019-06-11 09:32:43 -0300

    usando python como script carga

---
+++ scripts/init-produtos.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+import http.client
+import json
+
+connection = http.client.HTTPConnection('localhost:3000')
+
+headers = {'Content-type': 'application/json'}
+
+produtos = ["ajax", "alcool", "pinho-sol", "x14", "kiboa", "limpa-vidros", "anti-mofo-produto", "detergente", "papel-toalha", "guardanapo", "farelo-de-aveia", "salgadinho", "manteiga", "arroz", "feijao", "sal", "acucar-refinado", "acucar-mascavo", "acucar-demerara", "pipoca", "sobrecoxa", "sassami", "coxao-mole", "alcatra", "file-mignon", "maminha", "arroz-risoto", "biscoito-arroz", "biscoito-zooreta", "farinha-trigo", "cafe", "extrato-tomate", "leite-condensado", "passata", "creme-leite", "leite-coco", "extrato-baunilha", "fermento-biologico", "ovo", "sabonete", "sabonete-juju", "shampoo", "shampoo-juju", "lenco-umedecido", "papel-higienico", "gillete", "gillete-clarissa", "espuma-barbear", "escova-dentes", "escova-dentes-juju", "pasta-dentes", "pasta-dentes-juju", "condicionador", "condicionador-juju", "queijo-mussarela", "queijo-gorgonzola", "requeijao", "salame", "iogurte", "macarrao", "file-mignon-porco", "queijo-parmesao", "sal", "arroz", "canjica", "cafe", "leite", "nutella", "coco", "passata", "lava-louca", "desodorate", "agua-coco", "sabonete-liquido", "canela" 
+]
+for p in produtos:
+    connection.request('POST', '/produtos', json.dumps({p: {"sumario": {"obs":"obs-" + p, "preco": 9999999}, "historico": []}}), headers)
+    response = connection.getresponse()
+    response.read()
+
+connection.request('GET', '/produtos')
+response = connection.getresponse()
+print(response.read().decode())
