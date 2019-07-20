b72d61f237d627f8752718efb1dda708dfe1f692
danielhvs <danielhvs70@gmail.com>
2019-07-20 01:07:10 -0300

    removendo nao usados

--- src/servidor/handler.clj
+++ src/servidor/handler.clj
@@ -30,22 +30,8 @@
 (defn insert [item]
   (mc/insert-and-return db "produtos" item))
 
-(defn db-consulta-mercado [db nome]
-  (mc/find-maps db "mercado" {:nome nome}))
-
 (defn db-remove-tudo [db nome]
   (mc/remove db nome))
-
-(defn update-mercado [db {:keys [nome preco local data]}]
-  (let [m-banco (db-consulta-mercado db nome)]  
-    (if (empty? m-banco)
-        (let [m {:nome nome :comprar true :estoque 0 :preco preco :local local :data data}]
-          (mc/insert (:db (conecta-bd)) "mercado" m))
-        (let [mais-barato (if (< preco (:preco (first m-banco))) 
-                            {:preco preco :local local :data data} 
-                            (first m-banco))
-              m {:preco (:preco mais-barato) :local (:local mais-barato) :data (:data mais-barato)}]
-          (mc/update-by-id db "mercado" (:_id (first m-banco)) {$set m})))))
 
 ;; Utils
 (defn normaliza [nome]
@@ -67,13 +53,6 @@
 (defn parse-request-cadastro [json]
   (json/read-str (slurp json) :key-fn keyword :value-fn transforma-valor-request))
 
-(defn _merge [table-a table-b]
-  (->> (concat table-a table-b)  ;; stat with all the data
-       (sort-by :nome)           ;; split it into groups
-       (partition-by :nome)      ;; by produto
-       (map (partial apply merge))  ;; merge each group into a single map.
-))
-
 (defn filtra-produto [nome colecao] 
   (filter (fn [p] (= nome (:nome p))) colecao))
 
@@ -84,67 +63,12 @@
   (if (= chave :preco) (Float/valueOf valor) valor))
 
 ;; Servicos
-(defn consulta-mercado []
-  (-> (r/response (json/write-str (mc/find-maps (:db (conecta-bd)) "mercado") :value-fn transforma-id-para-string))
-      (r/header  "Access-Control-Allow-Origin" "*")))
-
-(defn consulta [nome]
-  (let [db (:db (conecta-bd))]
-    (-> (r/response (json/write-str (db-consulta-produto {:nome nome}) :value-fn transforma-id-para-string))
-        (r/header "Access-Control-Allow-Origin" "*"))))
-
-(defn consulta-produtos []
-  (let [db (:db (conecta-bd))]
-    (-> (r/response (json/write-str (db-consulta-produto {}) :value-fn transforma-id-para-string))
-        (r/header "Access-Control-Allow-Origin" "*"))))
-
 (defn remove-tudo []
   (let [db (:db (conecta-bd))]
     (db-remove-tudo db "mercado")
     (db-remove-tudo db "produtos")
     (-> (r/response "Removido")
         (r/header "Access-Control-Allow-Origin" "*"))))
-
-(defn salva-mercado [request]
-  (-> (r/response
-       (dosync (let [db (:db (conecta-bd))
-                     m (json/read-str (slurp (:body request)) :key-fn keyword)
-                     todo-mercado (mc/find-maps db "mercado")
-                     resultado (_merge todo-mercado m)]
-                 (mc/remove db "mercado")
-                 (mc/insert-batch db "mercado" resultado)
-                 (json/write-str (mc/find-maps db "mercado") :value-fn transforma-id-para-string))))
-      (r/header "Access-Control-Allow-Origin" "*")))
-
-(defn boot-mercado [request]
-  (-> (r/response
-       (dosync (let [db (:db (conecta-bd))
-                     todo-mercado (json/read-str (slurp (:body request)) :key-fn keyword)
-                     tudo (map #(assoc % :local "nenhum" :preco 99999999 :nome (normaliza (:nome %))) todo-mercado)]
-                 (mc/insert-batch db "mercado" tudo)
-                 (json/write-str (mc/find-maps db "mercado") :value-fn transforma-id-para-string))))
-      (r/header "Access-Control-Allow-Origin" "*")))
-
-(defn cadastra [request]
-  (-> (r/response
-       (dosync (let [p-request (parse-request-cadastro (:body request))
-                     p (assoc p-request :data (str (t/local-date)))
-                     db (:db (conecta-bd))]
-                 (update-mercado db p)
-                 (json/write-str (mc/insert-and-return db "produtos" p) :value-fn transforma-id-para-string))))
-      (r/header "Access-Control-Allow-Origin" "*")))
-
-(defn boot-cadastra [request]
-  (-> (r/response
-       (dosync (let [todos-produtos (map (fn [{:keys [nome preco] :as produto}]
-                                           (assoc produto :nome (normaliza nome) :preco (Float/valueOf preco))) 
-                                         (parse-request-cadastro (:body request)))
-                     tudo (map #(assoc % :data (str (t/local-date))) todos-produtos)
-                     db (:db (conecta-bd))]
-                 (doall (map #(update-mercado db %) tudo))
-                 (mc/insert-batch db "produtos" tudo)
-                 (json/write-str (db-consulta-produto {}) :value-fn transforma-id-para-string))))
-      (r/header "Access-Control-Allow-Origin" "*")))
 
 (defn opcoes []
   (-> (r/response "")
@@ -157,8 +81,6 @@
   (json/read-str (slurp (:body request)) :key-fn keyword :value-fn transforma-valor-request))
 
 ; {:nome "banana" :sumario "sumario" :melhor-preco "" :historico [{:preco :obs :local}]}}
-(def todos-produtos (atom {}))
-
 (defn get-produtos-nome [nome]
   (db-consulta-produto {:nome nome}))
 
@@ -227,21 +149,10 @@
   (POST "/produtos/:nome/sumario" [nome :as r] (produtos-nome-sumario (normaliza nome) r))
   (POST "/produtos/:nome/historico" [nome :as r] (produtos-nome-historico (normaliza nome) r))
   (GET "/produtos/:nome/:id" [nome id] (produtos-nome-id (normaliza nome) id))
-  ;(GET "/consulta/:nome" [nome] (consulta nome))
-  ;(GET "/consulta" [] (consulta-produtos))
-  ;(GET "/consulta-mercado" [] (consulta-mercado))
   (POST "/remove-tudo" [] (remove-tudo))
-  ;(POST "/cadastra" request (cadastra request))
-  ;(POST "/boot-cadastra" request (boot-cadastra request))
-  ;(POST "/boot-mercado" request (boot-mercado request))
-  ;(POST "/salva-mercado" request (salva-mercado request))
-  ;(OPTIONS "/cadastra" request (opcoes))
   (OPTIONS "/produtos" request (opcoes))
   (OPTIONS "/produtos/:nome/sumario" [nome :as r] (opcoes))
   (OPTIONS "/produtos/:nome/historico" [nome :as r] (opcoes))
-  ;(OPTIONS "/boot-cadastra" request (opcoes))
-  ;(OPTIONS "/boot-mercado" request (opcoes))
-  ;(OPTIONS "/salva-mercado" request (opcoes))
   (route/not-found "Not Found"))
 
 (defn wrap-debug [handler]
